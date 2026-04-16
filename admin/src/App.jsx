a79a9dc77699c9169140bfade62654352d10aaa6
razz399 <raiyayati@gmail.com>
2026-04-16 15:09:27 +0530

    Update API URLs to production endpoints

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login/Login";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://quickcrave-backend-jrl2.onrender.com";
   return (
     <div>
       <ToastContainer />
